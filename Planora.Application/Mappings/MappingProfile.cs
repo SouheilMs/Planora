5ce523b12d4f4780641d334f76f675da3a6c750e
Wassim Masmoudi <106668591+WasMas@users.noreply.github.com>
2026-04-14 18:25:19 +0100

    feat(mapping): add AssignedToName mapping for backlog items
    - Map AssignedToName from AssignedTo navigation property
    - Format full name as 'FirstName LastName' in DTOs for display
    - Enables frontend to show assigned user name without extra lookups

--- Planora.Application/Mappings/MappingProfile.cs
+++ Planora.Application/Mappings/MappingProfile.cs
@@ -7,6 +7,7 @@ using Planora.Application.DTOs.Projects;
 using Planora.Application.DTOs.Sprints;
 using Planora.Application.DTOs.Tasks;
 using Planora.Application.DTOs.Users;
+using Planora.Application.DTOs.Workspaces;
 using Planora.Domain.Entities;
 
 namespace Planora.Application.Mappings;
@@ -25,17 +26,23 @@ public class MappingProfile : Profile
 
         // Project mappings
         CreateMap<Project, ProjectDto>()
+            .ForMember(dest => dest.WorkspaceOwnerId, opt => opt.MapFrom(src => src.Workspace.OwnerId))
             .ForMember(dest => dest.ProjectManagerName, opt => opt.MapFrom(src =>
                 src.ProjectManager != null
                     ? $"{src.ProjectManager.FirstName} {src.ProjectManager.LastName}"
                     : string.Empty))
-            .ForMember(dest => dest.MemberCount, opt => opt.MapFrom(src => src.Members.Count))
+            .ForMember(dest => dest.MemberCount, opt => opt.MapFrom(src => src.Users.Count))
+            .ForMember(dest => dest.Members, opt => opt.MapFrom(src => src.Users))
             .ForMember(dest => dest.ProgressPercentage, opt => opt.Ignore());
 
         CreateMap<CreateProjectDto, Project>();
         CreateMap<UpdateProjectDto, Project>()
             .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
-
+        CreateMap<ProjectInvitation, ProjectInvitationDto>()
+            .ForMember(dest => dest.ProjectName, opt => opt.MapFrom(src => src.Project.Name))
+            .ForMember(dest => dest.UserFullName, opt => opt.MapFrom(src => $"{src.User.FirstName} {src.User.LastName}"))
+            .ForMember(dest => dest.UserEmail, opt => opt.MapFrom(src => src.User.Email ?? string.Empty))
+            .ForMember(dest => dest.InvitedByFullName, opt => opt.MapFrom(src => $"{src.InvitedByUser.FirstName} {src.InvitedByUser.LastName}"));
         // TaskItem mappings
         CreateMap<TaskItem, TaskDto>()
             .ForMember(dest => dest.AssignedToName, opt => opt.MapFrom(src =>
@@ -71,8 +78,45 @@ public class MappingProfile : Profile
                 src.Sprint != null ? src.Sprint.Name : null))
             .ForMember(dest => dest.ProjectName, opt => opt.MapFrom(src =>
                 src.Project != null ? src.Project.Name : null))
-            .ForMember(dest => dest.AssignedToName, opt => opt.Ignore()); // À mapper si nécessaire
+            .ForMember(dest => dest.AssignedToName, opt => opt.MapFrom(src =>
+                src.AssignedTo != null ? $"{src.AssignedTo.FirstName} {src.AssignedTo.LastName}" : string.Empty));
 
         CreateMap<CreateBacklogItemDto, BacklogItem>();
+
+        // Workspace mappings
+        CreateMap<Workspace, WorkspaceDto>()
+            .ForMember(dest => dest.OwnerName, opt => opt.MapFrom(src =>
+                src.Owner != null
+                    ? $"{src.Owner.FirstName} {src.Owner.LastName}"
+                    : string.Empty))
+            .ForMember(dest => dest.ProjectManagerId, opt => opt.MapFrom(src => src.ProjectManagerId))
+            .ForMember(dest => dest.ProjectManagerName, opt => opt.MapFrom(src =>
+                src.ProjectManager != null
+                    ? $"{src.ProjectManager.FirstName} {src.ProjectManager.LastName}"
+                    : string.Empty))
+            .ForMember(dest => dest.MemberCount, opt => opt.MapFrom(src => src.Members.Count))
+            .ForMember(dest => dest.ProjectCount, opt => opt.MapFrom(src => src.Projects.Count));
+
+        CreateMap<WorkspaceInvitation, WorkspaceInvitationDto>()
+            .ForMember(dest => dest.WorkspaceName, opt => opt.MapFrom(src => src.Workspace.Name));
+
+        CreateMap<ApplicationUser, WorkspaceInviteableUserDto>()
+            .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.Id))
+            .ForMember(dest => dest.FullName, opt => opt.MapFrom(src => $"{src.FirstName} {src.LastName}"))
+            .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email ?? string.Empty));
+
+        CreateMap<ProjectUser, ProjectMemberDto>()
+            .ForMember(dest => dest.FullName, opt => opt.MapFrom(src =>
+                src.User != null
+                    ? $"{src.User.FirstName} {src.User.LastName}"
+                    : string.Empty))
+            .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.User.Email ?? string.Empty));
+
+        CreateMap<WorkspaceUser, WorkspaceMemberDto>()
+            .ForMember(dest => dest.FullName, opt => opt.MapFrom(src =>
+                src.User != null
+                    ? $"{src.User.FirstName} {src.User.LastName}"
+                    : string.Empty))
+            .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.User.Email ?? string.Empty));
     }
 }
